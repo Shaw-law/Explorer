0ad5afa922fef3a4504d2dee0255c8dcc76cc291
Biswajit Shaw <biswajitshaw184@gmail.com>
2022-02-18 19:26:58 +0530

    initial commit.

--- Explorer.py
+++ Explorer.py
@@ -1,1 +1,32 @@
+import sys
 
+from PyQt5.QtWidgets import QApplication, QWidget, QTreeView, QFileSystemModel, QVBoxLayout
+from PyQt5.QtCore import QModelIndex
+
+class FileSystemView(QWidget):
+    def __init__(self, dir_path):
+        super().__init__()
+        appWidth = 800
+        appHeight = 300
+        self.setWindowTitle('File System Viewer')
+        self.setGeometry(300, 300, appWidth, appHeight)
+
+
+        self.model = QFileSystemModel()
+        self.model.setRootPath(dir_path)
+        self.tree = QTreeView()
+        self.tree.setModel(self.model)
+        self.tree.setRootIndex(self.model.index(dir_path))
+        self.tree.setColumnWidth(0, 250)
+        self.tree.setAlternatingRowColors (True)
+        
+        layout = QVBoxLayout()
+        layout.addWidget(self.tree)
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dir_path = r'C:/Users/Biswajit'
+    demo = FileSystemView(dir_path)
+    demo.show()
+    sys.exit(app.exec_())
